9846f4b772f31285aa355091ef1be89d5e4a4330
Gilbert Ardila <gilbertferney@gmail.com>
2023-11-21 08:08:55 -0500

    error main page

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -1,11 +1,10 @@
 'use client';
 
 const Error = ({ error, reset }) => {
-
-
     return (
         <div className='flex flex-col justify-center items-center pt-10'>
-            <h1 className='text-3xl mb-4'>Something went wrong</h1>
+            <h1 className='text-3xl mb-4'>It seems we have a problem here</h1>
+            <span className='text-3xl mb-4'>{error}</span>
             <button className='text-blue-500' onClick={() => reset()}>Try again</button>
         </div>
     )
