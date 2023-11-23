15666628d21110f42cfc505a06e03812b34e95d2
Gilbert Ardila <gilbertferney@gmail.com>
2023-11-23 07:02:01 -0500

    footer links to social media

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,24 +1,38 @@
 import CountryLookup from './CountryLookup';
 
+import { MdOutlineMailOutline } from 'react-icons/md';
+import { FaGithub } from 'react-icons/fa';
+import { CiLinkedin } from 'react-icons/ci';
+
 const Footer = () => {
     {/**Because the footer is absolute the layout on root must be relative */}
   return (
     
-    <footer className='absolute bottom-0 text-sm text-gray-500 bg-[#f2f2f2] w-full'>
+    <footer className='absolute bottom-0 text-sm text-gray-500 bg-[#f5f5f5] w-full'>
         <div className='border-b px-8 py-3'>
             <CountryLookup/>
         </div>
         <div className='flex flex-col sm:flex-row justify-between items-center px-8 py-3 space-y-7 sm:space-y-0'>
-            <ul  className='flex items-center space-x-6'>
-                <li className='link'>About</li>
-                <li className='link'>Advertising</li>
-                <li className='link'>Business</li>
-                <li className='link'>How Search Works</li>
+            <ul  className='flex items-center space-x-6 relative'>
+                <li className='link'>
+                    <a href='mailto:gilbertferney@gmail.com?subject=Quiero%20contactarte' target='_blank' rel='noopener noreferrer'><MdOutlineMailOutline className='anchor hover:bg-[#e14e41]'/></a>
+                </li>
+                <li className='link'>
+                    <a 
+                    className='anchor hover:bg-[#272C30]'
+                    href='https://github.com/GilbertArdila' target='_blank' rel='noopener noreferrer'><FaGithub className='text-2xl'/></a>
+                </li>
+                <li className='link'>
+                    <a 
+                    className='anchor hover:bg-[#0073B2]'
+                    href='https://www.linkedin.com/in/gilbert-ardila-cruz-652577210/' target='_blank' rel='noopener noreferrer'><CiLinkedin className='text-2xl'/></a>
+                </li>
+                
             </ul>
             <ul className='flex items-center space-x-6'>
-                <li className='link'>Privacy</li>
-                <li className='link'>Terms</li>
-                <li className='link'>Settings</li>
+                <li className='hover:underline cursor-pointer'>Privacy</li>
+                <li className='hover:underline cursor-pointer'>Terms</li>
+                <li className='hover:underline cursor-pointer'>Settings</li>
                 
             </ul>
         </div>
